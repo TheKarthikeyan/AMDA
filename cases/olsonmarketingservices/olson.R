ee2a3a6e191afe82269497dcada4c90636d154c9
Karthikeyan Malaisamy <karthikm@Karthikeyans-MacBook-Pro.local>
2019-07-03 13:02:59 +0530

    Latest commit

--- cases/olsonmarketingservices/olson.R
+++ cases/olsonmarketingservices/olson.R
@@ -1,4 +1,5 @@
 library(readr)
+library(MASS)
 OLSON <- read_csv("OLSON.csv")
 
 spec = c(train = .7, test = .1, validate = .2)
@@ -16,4 +17,14 @@ OlSON.TRAIN <- res$train
 OLSON.VALIDATION <- res$validate
 OLSON.TEST <- res$test
 
-bc <- box(lm(OlSON.TRAIN$CHARGES ~ OlSON.TRAIN$DAYS),lambda=seq(0,1,by=.1))
+model <- lm(OlSON.TRAIN$CHARGES ~ OlSON.TRAIN$DAYS)
+plot(model)
+
+bc <- boxcox(model,lambda=seq(-3,3))
+
+best.lam <- bc$x[which(bc$y==max(bc$y))]
+
+newModel <- lm((OlSON.TRAIN$CHARGES)^best.lam~OlSON.TRAIN$DAYS)
+plot(newModel)
+
+summary(newModel)
